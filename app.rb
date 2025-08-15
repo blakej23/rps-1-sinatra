d6f33b3a6ad7661e75b5049d256ab9b35c79e113
blakej23 <blakejones1991@gmail.com>
2025-08-15 01:37:24 +0000

    Rock working

--- app.rb
+++ app.rb
@@ -7,3 +7,31 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+
+moves = ["rock","paper","scissors"]
+
+comp_move = moves.sample
+
+if comp_move == "rock"
+  result = "tied"
+elsif comp_move == "paper"
+  result = "lost"
+else
+  result = "won"
+end
+
+"<h2>
+  We played rock!
+</h2>
+
+<h2>
+  They played #{comp_move}!
+</h2>
+
+<h2>
+  We #{result}!
+</h2>"
+
+end
